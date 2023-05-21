f1be302bb3aac894b5da4860f473cc9415d903fc
David Castro <david.dev.designer@gmail.com>
2023-05-21 11:19:54 -0300

    SearchWidget

--- ui/src/main/java/br/com/davidcastro/ui/widgets/SearchWidget.kt
+++ ui/src/main/java/br/com/davidcastro/ui/widgets/SearchWidget.kt
@@ -1,7 +1,6 @@
 package br.com.davidcastro.ui.widgets
 
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
